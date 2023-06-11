d6358e5a03a92e5231bc937df6cd1ddf88311ad8
HwangJunha <tarot1415@gmail.com>
2023-06-12 06:22:53 +0900

    [master] 6장 get, post, put 메소드 생성 및 테스트

---
+++ src/main/java/com/around/practice/controller/api/ApiItemController.java
@@ -0,0 +1,52 @@
+package com.around.practice.controller.api;
+
+import com.around.practice.dto.Item;
+import com.around.practice.repository.ItemRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@RestController
+public class ApiItemController {
+
+    private final ItemRepository itemRepository;
+
+    public ApiItemController(
+            ItemRepository itemRepository
+    ){
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping("/api/items")
+    Flux<Item> findAll(){
+        return this.itemRepository.findAll();
+    }
+
+    @GetMapping("/api/items/{id}")
+    Mono<Item> findOne(@PathVariable String id){
+        return this.itemRepository.findById(id);
+    }
+
+    @PostMapping("/api/items")
+    Mono<ResponseEntity<?>> addNewItem(@RequestBody Mono<Item> item){
+        return item.flatMap( s -> this.itemRepository.save(s))
+                .map(savedItem -> ResponseEntity
+                        .created(URI.create("/api/items/" + savedItem.getId()))
+                        .body(savedItem));
+    }
+
+    @PutMapping("/api/items/{id}")
+    Mono<ResponseEntity<?>> updateItem(
+        @RequestBody Mono<Item> item,
+        @PathVariable String id
+    ){
+        return item
+                .map(content -> new Item(id, content.getName(), content.getDescription(), content.getPrice()))
+                .flatMap(this.itemRepository::save)
+                .map(ResponseEntity::ok);
+    }
+
+}
